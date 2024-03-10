3fa32c58ed3baa7d53836b8f11ca07c5cbb35043
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-10 16:42:34 +0700

    feat/dasboard(header): Code UI header Create the header interface of the admin dashboard page

---
+++ resources/views/admin/layouts/admin.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang web của tôi</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/admin/style-header.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/logo.css')}}">
+
+    @yield('css')
+</head>
+<body>
+    @include('admin.blocks.header')
+    <main class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    {{-- <aside>
+                        @section('sidebar')
+                            @include('clients.blocks.sidebar')
+                        @show
+                    </aside> --}}
+                </div>
+                <div class="col-8">   
+                    {{-- <div class="content">
+                        @yield('content')
+                        @yield('title')
+                    </div> --}}
+                </div>
+            </div>
+        </div>
+    </main>
+        {{-- @include('clients.blocks.footer') --}}
+    {{-- <script src="{{asset('assets/admin/js/bootstrap.min.js')}}"></script> --}}
+    {{-- <script src="{{asset('assets/clients/js/custom.js')}}"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    @yield('js')
+    @stack('script')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
 });
