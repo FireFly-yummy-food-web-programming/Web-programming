de4a6f7299dfbb9b3718896ea9b5f2e2858a58ae
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-14 00:35:30 +0700

    feature/header: code header

---
+++ resources/views/clients/blocks/header-navbar.blade.php
@@ -0,0 +1,42 @@
+<div class="header-top d-flex" >
+    <div class="header-top-left">
+        <div class="phone">
+            <i class="fa fa-phone phone-icon" aria-hidden="true"></i>
+            <p>0123455644</p>
+        </div>
+        <div class="email">
+            <i class="fa fa-envelope-o email-icon" aria-hidden="true"></i>
+            <p>yummyrestaurant@yahoo.com</p>
+        </div>
+    </div>
+    <div class='header-top-right'>
+        <a href=''>
+            <div class='header-top-right-bg '>
+                <i class='fa fa-twitter twitter-icon' aria-hidden='true'></i>
+            </div>
+        </a>
+        <a href=''>
+            <div class='header-top-right-bg '>
+                <i class='fa fa-facebook facebook-icon' aria-hidden='true'></i>
+            </div>
+        </a>
+        <a href=''>
+            <div class='header-top-right-bg '>
+                <i class='fa fa-instagram instagram-icon' aria-hidden='true'></i>
+            </div>
+        </a>
+        <a href=''>
+            <div class='header-top-right-bg'>
+                <i class='fa fa-github github-icon' aria-hidden='true'></i>
+            </div>
+        </a>
+    </div>
+</div>
+<div class="header-navbarcontainer">
+    <div class="container-head">
+        @include('clients.blocks.logo')
+        <div class="header-left d-flex flex-row gap-3">
+            
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('users')->group(function () {
+    Route::get('/home', function () {
+        return view('clients.layouts.layout');
+    });
+});
